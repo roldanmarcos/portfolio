b8e8e3913bada71c87d4c1774be042b6d8be9b6c
roldanmarcos <guillermoroldan0@gmail.com>
2026-01-02 17:07:57 -0300

    portfolio v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import WelcomePage from './pages/WelcomePage/WelcomePage'
 import FrontendPage from './pages/FrontendPage/FrontendPage';
